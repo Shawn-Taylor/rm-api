cffd7576844faa7d28759483ade8bde7936572be
Shawn Taylor <taylorshawn@hotmail.com>
2020-09-07 09:34:17 -0400

    Initial commit

---
+++ event.py
@@ -0,0 +1,11 @@
+# Event API code
+ 
+def get_events():    
+    # query the db and return a list of events for a customer
+    pass
+ 
+def update_event();
+    pass
+
+def add_event():
+    pass
---
+++ registration.py
@@ -0,0 +1,11 @@
+# Event API code
+ 
+def get_registration():    
+    # query the db and return a list of events for a customer
+    pass
+ 
+def update_registration();
+    pass
+
+def add_registration():
+    pass
